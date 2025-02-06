70707f5d543a953744a27129812a615ebef0d914
JAYDEEPSOLANKI45 <jaydeepsolanki4567@gmail.com>
2025-02-06 19:12:14 +0530

    add all the dependencies, setup mongodb cluster and session store

--- index.js
+++ index.js
@@ -8,5 +8,47 @@ const path=require("path")
 const mongoStore=require("connect-mongo")
 const mongoose=require("mongoose");
 const methodOverride=require("method-override");
+const QueryString = require("qs")
+const exp = require("constants")
 app.use(methodOverride("_method"));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge:3600 * 1000,
+        httpOnly: true
+    },
+    store:mongoStore.create({
+        crypto:process.env.SECRET,
+        mongoUrl:process.env.MONGODB_URI,
+        touchAfter:3600
+    })
+}));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public/css")))
+app.use(express.static(path.join(__dirname,"public/js")))
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URI);
+}
+main().then(()=>console.log("Connected")).catch(()=>console.log("Error occured while connecting with the mongodb cluster"));
+
+app.get("/",(req,res)=>
+{
+    res.redirect(`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENTID}&response_type=code&state=${process.env.RANDOM}&redirect_uri=${process.env.REDIRECT_URI}&duration=permanent&scope=${process.env.SCOPE}`)
+})
+
+app.get("/redirect",(req,res)=>{
+    req.session.authcode=req.query.code;
+    res.send(req.query.code);
+})
+
+app.listen(8080,(req,res)=>{
+    console.log("Listening");
+})
