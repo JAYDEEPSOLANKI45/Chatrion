fd088ee089d44e7f44e87b8783704347e2b212e7
JAYDEEPSOLANKI45 <jaydeepsolanki4567@gmail.com>
2025-02-08 13:26:27 +0530

    add search subreddit names and info routes

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -16,7 +16,7 @@ router.get("/",isLogined,wrapAsync(async(req,res,next)=>{
 }));
 
 // search for reddit names (list of names) that begins with the query string
-router.get("/subreddit",isLogined,wrapAsync(async(req,res,next)=>{
+router.get("/subreddit_names",isLogined,wrapAsync(async(req,res,next)=>{
     let {query}=req.query;
     let headers={
         "Authorization":`Bearer ${req.session.access_token}`,
@@ -25,4 +25,17 @@ router.get("/subreddit",isLogined,wrapAsync(async(req,res,next)=>{
     const data=await axios.get(`https://oauth.reddit.com/api/search_reddit_names?query=${query}&include_over_18=false&exact=false`,{headers});
     res.send(data.data);
 }));
+
+//search for detailed subreddit information
+router.get("/subreddit_info",isLogined,wrapAsync(async(req,res,next)=>{
+    let {query}=req.query;
+    let headers={
+        "Authorization":`Bearer ${req.session.access_token}`,
+        'User-Agent': 'Chatrion'
+    };
+    const body = `query=${query}&include_over_18=false&exact=false`;
+    const data=await axios.post(`https://oauth.reddit.com/api/search_subreddits`,body,{headers});
+    res.send(data.data);
+}));
+
 module.exports=router;
